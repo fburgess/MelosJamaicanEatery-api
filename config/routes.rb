bd780ba888c8aa2f6c42ae5def717d8d6853eadd
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-05-24 15:28:56 -0400

    seeds done..

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :order_histories
+  resources :menu_items
+  resources :reviews
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
